f46dea462f38938d390fa270da26914c6440994a
yuizhang <yuizh@foxmail.com>
2019-12-04 15:07:09 +0800

    add tests for new demand

--- parking-lot/parking-lot-test/ParkingLotBrotherTest.cs
+++ parking-lot/parking-lot-test/ParkingLotBrotherTest.cs
@@ -117,5 +117,47 @@ namespace parking_lot_test
             var exception = Assert.Throws<InvalidTicketException>(() => brother.Pick(ticket));
             Assert.Equal("invalid ticket", exception.Message);
         }
+
+        [Fact]
+        public void should_park_car_to_the_plot_with_the_maximum_number_of_empty_slots_when_given_a_car_and_multiple_available_parking_lots()
+        {
+            var parkingLot1 = new ParkingLot(1);
+            var parkingLot2 = new ParkingLot(3);
+            var parkingLot3 = new ParkingLot(2);
+            
+            var bro = new ParkingBrother(new List<ParkingLot>
+            {
+                parkingLot1,
+                parkingLot2,
+                parkingLot3,
+            });
+
+            bro.Park(new Car());
+            
+            Assert.Equal(0, parkingLot1.CurrentCount());
+            Assert.Equal(1, parkingLot2.CurrentCount());
+            Assert.Equal(0, parkingLot3.CurrentCount());
+        }
+        
+        [Fact]
+        public void should_park_car_to_the_first_plot_when_more_than_two_plots_have_same_number_of_empty_slots_as_maximum()
+        {
+            var parkingLot1 = new ParkingLot(2);
+            var parkingLot2 = new ParkingLot(3);
+            var parkingLot3 = new ParkingLot(3);
+            
+            var bro = new ParkingBrother(new List<ParkingLot>
+            {
+                parkingLot1,
+                parkingLot2,
+                parkingLot3,
+            });
+
+            bro.Park(new Car());
+            
+            Assert.Equal(0, parkingLot1.CurrentCount());
+            Assert.Equal(1, parkingLot2.CurrentCount());
+            Assert.Equal(0, parkingLot3.CurrentCount());
+        }
     }
 }
